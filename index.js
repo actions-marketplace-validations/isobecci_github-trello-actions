d133847a3eb9bf6cc3af25797301fbde6facba1c
isobecci <isobecci@users.noreply.github.com>
2022-05-31 15:01:56 +0900

    fix regExp

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // import core from '@actions/core';
 import github from '@actions/github';
-import { getTrello, createACardWithParams, createAttachmentOnCard, moveACard } from './_trello.js';
+import {
+  getTrello,
+  createACardWithParams,
+  createAttachmentOnCard,
+  moveACard,
+} from './_trello.js';
 
 const trelloApiKey = process.env['TRELLO_API_KEY'];
 const trelloApiToken = process.env['TRELLO_API_TOKEN'];
@@ -16,13 +21,31 @@ const pullRequest = github.context.payload.pull_request;
 
 (async () => {
   try {
-    const members = await getTrello('board', boardId, 'members', trelloApiKey, trelloApiToken);
-    const labels = await getTrello('board', boardId, 'labels', trelloApiKey, trelloApiToken);
+    const members = await getTrello(
+      'board',
+      boardId,
+      'members',
+      trelloApiKey,
+      trelloApiToken,
+    );
+    const labels = await getTrello(
+      'board',
+      boardId,
+      'labels',
+      trelloApiKey,
+      trelloApiToken,
+    );
 
     if (issue) {
-      const memberIds = members.filter(({ username }) => issue.assignees.find(({ login }) => login === username)).map(({ id }) => id);
+      const memberIds = members
+        .filter(({ username }) =>
+          issue.assignees.find(({ login }) => login === username),
+        )
+        .map(({ id }) => id);
       // TODO: replace Github login to Trello username
-      const labelIds = labels.filter(label => issue.labels.find(({ name }) => name === label.name)).map(({ id }) => id);
+      const labelIds = labels
+        .filter((label) => issue.labels.find(({ name }) => name === label.name))
+        .map(({ id }) => id);
       const params = {
         number: issue.number,
         title: issue.title,
@@ -30,38 +53,62 @@ const pullRequest = github.context.payload.pull_request;
         memberIds,
         labelIds,
       };
-      const card = await createACardWithParams(todoListId, params, trelloApiKey, trelloApiToken);
-      await createAttachmentOnCard(card.id, issue.html_url, trelloApiKey, trelloApiToken); 
+      const card = await createACardWithParams(
+        todoListId,
+        params,
+        trelloApiKey,
+        trelloApiToken,
+      );
+      await createAttachmentOnCard(
+        card.id,
+        issue.html_url,
+        trelloApiKey,
+        trelloApiToken,
+      );
       console.log('issue:open', issue.created_at);
     }
-    
+
     if (pullRequest) {
-      const issueNumbers = pullRequest.body && pullRequest.body.match(/(#[0-9]+?)/g);
+      const issueNumbers =
+        pullRequest.body && pullRequest.body.match(/(#[0-9]+?)/g);
       if (issueNumbers && issueNumbers.length > 0) {
-        const [fromListId, toListId] = (pullRequest.closed_at)
+        const [fromListId, toListId] = pullRequest.closed_at
           ? [doingListId, doneListId]
           : [todoListId, doingListId];
         // get cards
-        const cards = await getTrello('lists', fromListId, 'cards?open', trelloApiKey, trelloApiToken);
+        const cards = await getTrello(
+          'lists',
+          fromListId,
+          'cards?open',
+          trelloApiKey,
+          trelloApiToken,
+        );
         const targetCards = new Set();
-        for (const issueNumber of issueNumbers) {
-          const { id: cardId } = cards.find(({ name }) => new RegExp(`\[${ issueNumber }\]`, 'i').test(name)) || { id: null};
+        for (let issueNumber of issueNumbers) {
+          const { id: cardId } = cards.find(({ name }) =>
+            new RegExp(`\\[${issueNumber}\\]`, 'i').test(name),
+          ) || { id: null };
           cardId && targetCards.add(cardId);
         }
-        targetCards.size && targetCards.forEach(async (cardId) => {
-          if (pullRequest.closed_at) {
-            console.log('pullRequest:closed', pullRequest.closed_at);
-          } else {
-            console.log('pullRequest:open', pullRequest.created_at);
-            // add attachment (PR) to card
-            await createAttachmentOnCard(cardId, pullRequest.html_url, trelloApiKey, trelloApiToken);
-          }
-          // move card
-          await moveACard(cardId, toListId, trelloApiKey, trelloApiToken);
-        });
+        targetCards.size &&
+          targetCards.forEach(async (cardId) => {
+            if (pullRequest.closed_at) {
+              console.log('pullRequest:closed', pullRequest.closed_at);
+            } else {
+              console.log('pullRequest:open', pullRequest.created_at);
+              // add attachment (PR) to card
+              await createAttachmentOnCard(
+                cardId,
+                pullRequest.html_url,
+                trelloApiKey,
+                trelloApiToken,
+              );
+            }
+            // move card
+            await moveACard(cardId, toListId, trelloApiKey, trelloApiToken);
+          });
       }
     }
-
   } catch (error) {
     throw new Error(error);
     // core.setFailed(error.message);
