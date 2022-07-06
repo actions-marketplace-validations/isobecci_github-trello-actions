66cc70176e5386b2041fa6c38e35ce8a4d15ecfd
isobecci <isobecci@users.noreply.github.com>
2022-07-06 15:13:30 +0900

    fix env key

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { doAction } from './_action.js';
 const trelloApiKey = process.env['TRELLO_API_KEY'];
 const trelloApiToken = process.env['TRELLO_API_TOKEN'];
 const boardId = process.env['TRELLO_BOARD_ID'];
-const todoListId = process.env['TRELLO_TODO_ID'];
+const todoListId = process.env['TRELLO_TODO_LIST_ID'];
 const doingListId = process.env['TRELLO_DOING_LIST_ID'];
 const doneListId = process.env['TRELLO_DONE_LIST_ID'];
 
